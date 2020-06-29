5341ac827ee69fb5d22b6c9119f6be6a4deb7b23
maulayyacyber <ridaulmaulayya@gmail.com>
2020-06-29 12:46:00 +0700

    add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:learn_multiple_page/login_page.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
 }
---
+++ lib/mian_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:learn_multiple_page/second_page.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Goto To Second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
